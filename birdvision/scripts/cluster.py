60e3aab5ca2ef71ea27b0d5b6fe61de484948c1b
Emily A. Bellows <emily.a.bellows@gmail.com>
2020-11-15 17:43:38 -0500

    Added option to cluster by color, reformatted

--- birdvision/scripts/cluster.py
+++ birdvision/scripts/cluster.py
@@ -1,14 +1,16 @@
-from birdvision.config import configure
+from pathlib import Path
+
 import click
+import cv2
 from sklearn.cluster import MiniBatchKMeans
 from tqdm import tqdm
-from pathlib import Path
+
+from birdvision.config import configure
 from birdvision.node import Node
-import cv2
 
 
 def chunk(seq, size):
-    return (seq[pos:pos+size] for pos in range(0, len(seq), size))
+    return (seq[pos:pos + size] for pos in range(0, len(seq), size))
 
 
 def load_image(path):
@@ -19,7 +21,7 @@ def load_image(path):
 
 
 def for_every_image(file_chunks, f):
-    for i, file_chunk in enumerate(tqdm(file_chunks)):
+    for i, file_chunk in enumerate(tqdm(file_chunks, desc=f.__name__)):
         images = []
         for path in file_chunk:
             image = load_image(path)
@@ -30,14 +32,25 @@ def for_every_image(file_chunks, f):
         f(i, images)
 
 
+def images_for_cluster_type(images, cluster_type):
+    if cluster_type == 'gray':
+        return [node.gray.thumbnail32.image.flatten() for (_, node) in images]
+    elif cluster_type == 'color':
+        return [node.thumbnail32.image.flatten() for (_, node) in images]
+    else:
+        raise Exception(f'unknown cluster_type option "{cluster_type}"')
+
+
 @click.command()
 @click.option('--clusters', default=20, help="The number of clusters")
+@click.option('--cluster-gray', 'cluster_type', flag_value='gray', help="Cluster grayscale images", default=True)
+@click.option('--cluster-color', 'cluster_type', flag_value='color', help="Cluster color images")
 @click.option('--full', 'output', flag_value='full', help="Write the original image into each bucket", default=True)
 @click.option('--gray', 'output', flag_value='gray', help="Write the full grayscale image into each bucket")
 @click.option('--thumb', 'output', flag_value='thumb', help="Write the thumbnail grayscale image into each bucket")
 @click.argument('src')
 @click.argument('dst')
-def cluster_images(src, dst, clusters, output):
+def cluster_images(src, dst, clusters, cluster_type, output):
     """
     When manually labelling data, it's often helpful to run K-Means on it to do the bulk of the work for you.
 
@@ -48,15 +61,15 @@ def cluster_images(src, dst, clusters, output):
     dst = Path(dst)
 
     k_means = MiniBatchKMeans(n_clusters=clusters)
-    file_chunks = list(chunk(list(src.glob('**/*')), size=100))
+    file_chunks = list(chunk(list(src.glob('**/*')), size=clusters * 5))
     predictions = []
 
     def partial_fit(_i, images):
-        resized = [node.gray.thumbnail32.image.flatten() for (_, node) in images]
+        resized = images_for_cluster_type(images, cluster_type)
         k_means.partial_fit(resized)
 
     def predict(_i, images):
-        resized = [node.gray.thumbnail32.image.flatten() for (_, node) in images]
+        resized = images_for_cluster_type(images, cluster_type)
         predictions.append(k_means.predict(resized))
 
     def write_buckets(i, images):
@@ -74,7 +87,7 @@ def cluster_images(src, dst, clusters, output):
             elif output == 'thumb':
                 cv2.imwrite(image_path, node.gray.thumbnail32.image)
             else:
-                raise Exception(f'output option "{output}" not understood')
+                raise Exception(f'unknown output option "{output}"')
 
     for_every_image(file_chunks, partial_fit)
     for_every_image(file_chunks, predict)
